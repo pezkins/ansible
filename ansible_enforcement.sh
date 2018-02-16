9258ed5b67908b7413dc6b68b621e3338a3a05f5
Alex <pezkins@gmail.com>
2018-02-16 11:31:48 -0500

    testing

--- ansible_enforcement.sh
+++ ansible_enforcement.sh
@@ -8,16 +8,16 @@ LOGFILE=$SOURCE_DIR/ansible.log
 PLAYBOOK_FILE=/etc/ansible/playbooks/enforcement_test.yml
 
 cd $DESTINATION_DIR
-ansible-playbook $PLAYBOOK_FILE --check >> $LOGFILE
+ansible-playbook $PLAYBOOK_FILE --check ## >> $LOGFILE
 
 if [ $? -eq 0 ]
 then
-  echo "Check completed without error running full command" >> $LOGFILE
+  echo "Check completed without error running full command" ## >> $LOGFILE
 cd $DESTINATION_DIR
-ansible-playbook $PLAYBOOK_FILE $TOGGLE_CHECK >> $LOGFILE
+ansible-playbook $PLAYBOOK_FILE $TOGGLE_CHECK ## >> $LOGFILE
 
 else
-  echo "Check Failed please review ansible syntax" >> $LOGFILE
+  echo "Check Failed please review ansible syntax" ## >> $LOGFILE
 fi
 
 #echo "Running... " >> $LOGFILE
