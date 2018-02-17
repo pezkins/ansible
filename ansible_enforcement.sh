3e662b3da23b151f86a3304d739431f7c8af5f18
Alex <pezkins@gmail.com>
2018-02-16 21:42:12 -0500

    testing out elif

--- ansible_enforcement.sh
+++ ansible_enforcement.sh
@@ -10,15 +10,24 @@ LOGFILE=$SOURCE_DIR/ansible.log
 PLAYBOOK_PATH=/etc/ansible/playbooks/enforcement_test.yml
 #PLAYBOOK_PATH=/etc/ansible/playbooks/$PLAYBOOK.yml
 
-cd $DESTINATION_DIR
-ansible-playbook $PLAYBOOK_PATH --syntax-check ## >> $LOGFILE
-
-if [ $? -eq 0 ]
-then
-  echo "Check completed without error running full command" ## >> $LOGFILE
-cd $DESTINATION_DIR
-ansible-playbook $PLAYBOOK_PATH $TOGGLE_CHECK ## >> $LOGFILE
-
+if [$TOGGLE_CHECK -ne "--check"]
+	then
+	echo "Only argument allowed is --check"
+elif [ $TOGGLE_CHECK "-eq -h" ]
+	then
+	echo "Only argument allowed is --check"
+elif [ $TOGGLE_CHECK -eq --help ]
+	then
+	echo "Only argument allowed is --check"
+elif [ $TOGGLE_CHECK -eq --check ]
+	then
+	cd $DESTINATION_DIR
+	ansible-playbook $PLAYBOOK_PATH --syntax-check ## >> $LOGFILE
+elif [ $? -eq 0 ]
+	then
+	echo "Check completed without error running full command" ## >> $LOGFILE
+	cd $DESTINATION_DIR
+	ansible-playbook $PLAYBOOK_PATH $TOGGLE_CHECK ## >> $LOGFILE
 else
   echo "Check Failed please review ansible syntax" ## >> $LOGFILE
 fi
@@ -28,6 +37,8 @@ fi
 #ansible-playbook $PLAYBOOK_FILE >> $LOGFILE
 #SORTIDA=$?
 #echo OK >> $LOGFILE
+
+#if arg = check do wh atever; elseif arg =-h do something else elseif arg=--help" do smethhing..
 
 #!
 #### ansible command run ####
