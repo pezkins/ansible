3167d552439dac3ac5b49f821c1d6edd52866b24
Alex <pezkins@gmail.com>
2018-02-16 23:33:33 -0500

    trying something new lol

--- testing_script.sh
+++ testing_script.sh
@@ -10,23 +10,24 @@ LOGFILE=$SOURCE_DIR/ansible.log
 PLAYBOOK_PATH=/etc/ansible/playbooks/enforcement_test.yml
 #PLAYBOOK_PATH=/etc/ansible/playbooks/$PLAYBOOK.yml
 
-cd $DESTINATION_DIR
-ansible-playbook $PLAYBOOK_PATH --syntax-check ## >> $LOGFILE
-
-if [ $? -eq 0 ]
-then
-  echo "Check completed without error running full command" ## >> $LOGFILE
-
-elif [ $TOGGLE_CHECK -eq "-h" ] || [ $TOGGLE_CHECK -eq "--help" ] || ([$TOGGLE_CHECK -ne "--check"] || [$TOGGLE_CHECK -ne ""])
+if [ $TOGGLE_CHECK -eq "-h" ] || [ $TOGGLE_CHECK -eq "--help" ]
 	then
 	echo "Only argument allowed is --check or no argument"
 
-cd $DESTINATION_DIR
-ansible-playbook $PLAYBOOK_PATH $TOGGLE_CHECK ## >> $LOGFILE
+elif [$TOGGLE_CHECK -ne "--check"] || [$TOGGLE_CHECK -ne ""]
+	then
+	echo "Only argument allowed is --check or no argument"
 
+elif [$TOGGLE_CHECK -eq "--check"] || [$TOGGLE_CHECK -eq ""]
+	then
+	cd $DESTINATION_DIR
+	ansible-playbook $PLAYBOOK_PATH --syntax-check ## >> $LOGFILE
+	if [ $? -eq 0 ]
+		then
+		echo "Check completed without error running full command" ## >> $LOGFILE
+		cd $DESTINATION_DIR
+		ansible-playbook $PLAYBOOK_PATH $TOGGLE_CHECK ## >> $LOGFILE
 else
   echo "Check Failed please review ansible syntax" ## >> $LOGFILE
 fi
-
-
 
