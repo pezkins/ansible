83a079c53b758231e2b954c11593e4fffba4cac9
Alex <pezkins@gmail.com>
2018-02-17 00:35:48 -0500

    creating the new script

--- ansible_enforcement.sh
+++ ansible_enforcement.sh
@@ -10,31 +10,34 @@ LOGFILE=$SOURCE_DIR/ansible.log
 PLAYBOOK_PATH=/etc/ansible/playbooks/enforcement_test.yml
 #PLAYBOOK_PATH=/etc/ansible/playbooks/$PLAYBOOK.yml
 
-cd $DESTINATION_DIR
-ansible-playbook $PLAYBOOK_PATH --syntax-check ## >> $LOGFILE
+if { [ "$TOGGLE_CHECK" = "-h" ] || [ "$TOGGLE_CHECK" = "--help" ]; };
+	then
+	echo "
+-h, --help		This will show you this message
+--check    		This will NOT enforce the playbook just displays a dry run
+NULL			Not typing any argument will enforce the playbook
+"
 
-if [ $? -eq 0 ]
-then
-  echo "Check completed without error running full command" ## >> $LOGFILE
-cd $DESTINATION_DIR
-ansible-playbook $PLAYBOOK_PATH $TOGGLE_CHECK ## >> $LOGFILE
+elif { [ "$TOGGLE_CHECK" != "--check" ] || [ "$TOGGLE_CHECK" -ne 0 ]; };
+	then
+	echo "
+!!!!!!!!!! Please use -h, --help to see a list of accepted arguments !!!!!!!!!!
+"
 
-else
-  echo "Check Failed please review ansible syntax" ## >> $LOGFILE
+elif { [ "$TOGGLE_CHECK" = "--check" ] || [ "$TOGGLE_CHECK" -eq 0 ]; };
+	then
+	echo "Running Playbook"
+	cd $DESTINATION_DIR
+	ansible-playbook $PLAYBOOK_PATH --syntax-check ## >> $LOGFILE
+	if [ $? -eq 0 ]
+		then
+		echo "Check completed without error running full command" ## >> $LOGFILE
+		cd $DESTINATION_DIR
+		ansible-playbook $PLAYBOOK_PATH $TOGGLE_CHECK ## >> $LOGFILE
+	else
+	echo "Check Failed please review ansible syntax" ## >> $LOGFILE
+	fi
 fi
 
-#echo "Running... " >> $LOGFILE
-#cd $DESTINATION_DIR
-#ansible-playbook $PLAYBOOK_FILE >> $LOGFILE
-#SORTIDA=$?
-#echo OK >> $LOGFILE
-
-#!
-#### ansible command run ####
-#
-#if "ansible-playbook --check /etc/ansible/playbooks/enforcement_test.yml"; then
-#        "ansible-playbook /etc/ansible/playbooks/enforcement_test.yml"
-#else
-#        echo "Check Failed!"
-#fi
-
+### --diff			when changing (small) files and templates, show the differences in those files
+### DIFF_CHECK=$2
